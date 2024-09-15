eeb758dd0713e8c605cb7813d9cfb549cca4ee4f
mambo-mnmc <consult.mnmc@gmail.com>
2024-09-15 11:38:48 +0200

    Create testing.py

---
+++ My Python Basics/testing.py
@@ -0,0 +1,24 @@
+def Returnsum(RU_STUFF):
+    
+    sumint=0
+    for i in RU_STUFF:
+        sumint=sumint+ RU_STUFF[I]
+
+    return Returnsum
+RU_STUFF=[]
+st = {"Owner":"Mr Kapenya","Color":"white","Brand":"IPSUM","Myear":2008}
+sd = {"Owner":"Mr Masivi","Color":"black","Brand":"NOTSURE","Myear":2012}
+rd = {"Owner":"Mrs Simate","Color":"Grey","Brand":"TOYATA","Myear":2008}
+RU_STUFF.append(dict(st ))
+RU_STUFF.append(dict(sd ))
+RU_STUFF.append(dict(rd ))
+print(RU_STUFF)
+for i in RU_STUFF:
+    Cyear=int(input("Enter the Current year: "))
+    Myear=int(input("Enter the Manufacturing year: "))
+age=Cyear-Myear   
+Average = Returnsum//age
+print("The Average of the car's age is",Average)
+    
+    
+    
