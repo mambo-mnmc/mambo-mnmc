535b191074237615167f6a085403786ef99b1d8f
mambo-mnmc <consult.mnmc@gmail.com>
2024-09-15 11:38:56 +0200

    Create numbers divisible by 4.py

---
+++ My Python Basics/numbers divisible by 4.py
@@ -0,0 +1,8 @@
+import os
+while True:
+    input("press enter to continue")
+    for nd4 in range (11+1, 111+1, 4):
+        print (nd4)
+    if input("do you want to continue?(Yes/No)").upper() == "NO":
+        break
+os.system("pause")
