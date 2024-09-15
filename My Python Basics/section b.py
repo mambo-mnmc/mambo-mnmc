6b9f11fe8cd64c9275b38d8a696f2836ddc88060
mambo-mnmc <consult.mnmc@gmail.com>
2024-09-15 11:37:03 +0200

    Create section b.py

---
+++ My Python Basics/section b.py
@@ -0,0 +1,23 @@
+numb=[]
+start=-21
+end=42
+step=7
+index=start
+all=0
+while index<=end:
+    numb.append(index)
+    index=index+step
+for index in range (0, 10, 1):
+    all=all+numb[index]
+
+print(start)
+print(end)
+print(end)
+
+if b>d[2]:
+    print(d[0])
+elif b == d[2]:
+    print(d[3])
+else:
+    print("we are sorry")
+    print(d[0])
